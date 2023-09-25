d53534189a5185a864856bddd72a095d3d62c748
thitipong64349440 <thitipongr64@nu.ac.th>
2023-09-25 16:15:55 +0700

    .

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import UserIndex from '@/components/User/Index'
+import UserCreate from '@/components/User/CreateUser'
+import UserEdit from '@/components/User/EditUser'
+import UserShow from '@/components/User/ShowUser'
 
 Vue.use(Router)
 
@@ -10,6 +16,31 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path:'/teat',
+      name:'test',
+      component: Test
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: UserIndex
+    },
+    {
+      path: '/user/create',
+      name: 'users-create',
+      component: UserCreate
+    },
+    {
+      path: '/user/edit',
+      name: 'user-edit',
+      component: UserEdit
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserShow
+    }, 
   ]
 })
